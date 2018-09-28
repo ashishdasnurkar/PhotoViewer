02fc7d851d49f0ff19bd62ea483238411d09631c
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2018-09-29 02:13:59 +0800

    Initial Commit

---
+++ StormViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StormViewer
+//
+//  Created by Ashish Dasnurkar on 2018/9/29.
+//  Copyright © 2018 EthSu Technology. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Ashish Dasnurkar on 2018/9/29.
+//  Copyright © 2018 EthSu Technology. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
